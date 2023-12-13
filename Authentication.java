2d4a6c2b7d404b3482934b7d44508a672e0ce41b
Shashwat <shashwat.bhandari.14@gmail.com>
2023-12-13 10:56:17 +0800

    Add encryption and decryption

--- Authentication.java
+++ Authentication.java
@@ -1,36 +1,55 @@
 /*
  * Authentication class for password protection
+ *
+ * Specific snippets from sources
+ * Closing a JFrame - https://stackoverflow.com/questions/1234912/how-to-programmatically-close-a-jframe
  */
 
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class Authentication implements ActionListener {
 
     // global password field attribute for actionPerformed method
-    private JPasswordField passwordField = new JPasswordField(20);
-    private String password = "";
+    private final JPasswordField passwordField;
+
+    // global result label field for updates
+    private final JLabel result;
+
+    // determines access
+    private boolean success = false;
+
+    // encoded actual password
+    private final static String correct = "password"; // get password from database
+
+    // main frame
+    JFrame frame = new JFrame("Authentication");
 
     // constructor
     public Authentication() {
 
-        // main frame
-        JFrame frame = new JFrame("Authentication");
-
         // main panel
+        JPanel mainPanel = new JPanel();
+
+        // panel for input
         JPanel panel = new JPanel();
 
+        // panel to inform user on status
         JPanel resultPanel = new JPanel();
 
-        // password label, and button
+        // password label, result label and button
         JLabel passwordLabel = new JLabel("Password: ");
         JButton submitButton = new JButton("Submit");
 
-        // passwordLabel.setFont(new Font("Verdana", Font.PLAIN, 12));
-
         // add action listeners to password field and button
+        passwordField = new JPasswordField(20);
         passwordField.addActionListener(this);
         submitButton.addActionListener(this);
 
@@ -39,48 +58,113 @@ public class Authentication implements ActionListener {
         panel.add(passwordField);
         panel.add(submitButton);
 
+        result = new JLabel("Enter Password");
+        resultPanel.add(result);
+
+        // merge two panels vertically
+        mainPanel.add(panel);
+        mainPanel.add(resultPanel);
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
         // configure frame
         frame.setResizable(false); // disables maximize button
         frame.setVisible(true);
-        frame.setLayout(new CardLayout());
-        frame.add(panel);
+        frame.setLayout(new FlowLayout());
+        frame.add(mainPanel);
         frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); // puts frame in the middle
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
     // validate password
-    private boolean validatePassword(char[] password) {
-        if (password.length < 8 || password.length > 20) return false;
-        return true;
+    private boolean checkPassword(String password) {
+        return password.equals(correct);
+    }
+
+    // encrypt password
+    // need the same key for decryption - key only changes when password changes
+    // Raised exceptions include: NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+    // IllegalBlockSizeException, BadPaddingException
+    private String encryptPassword(String password, SecretKey secretKey, IvParameterSpec ivParameterSpec) throws Exception {
+
+        // most of the code from Bard for encryption
+        // given sources:
+        // https://www.baeldung.com/java-aes-encryption-decryption,
+        // https://www.tutorialspoint.com/java_cryptography/index.htm,
+        // https://jenkov.com/tutorials/java-cryptography/index.html
+
+        // convert password to byte array
+        byte[] plainText = password.getBytes(StandardCharsets.UTF_8);
+
+        // initialize cipher object
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec); // int, key, iv
+
+        // encrypt and encode
+        byte[] encryptedText = cipher.doFinal(plainText);
+        String encodedText = Base64.getEncoder().encodeToString(encryptedText);
+
+        System.out.println("Encrypted: " + encodedText);
+        return encodedText;
+    }
+
+    // painstakingly reverse-engineered
+    private String decryptPassword(String encodedText, SecretKey secretKey, IvParameterSpec ivParameterSpec) throws Exception {
+        // initializing cipher object
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec); // initialization vector required
+
+        byte[] encryptedText = Base64.getDecoder().decode(encodedText);
+        byte[] decryptedText = cipher.doFinal(encryptedText);
+        String result = new String(decryptedText, StandardCharsets.UTF_8);
+
+        System.out.println("Decrypted: " + result);
+        return result;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         char[] input = passwordField.getPassword();
 
-        if (!validatePassword(input)) {
-            System.out.println("ERROR - invalid password length");
-
-            // update screen
-        } else {
-            // give access
-        }
-
-
         // resetting every time action is performed
-        password = "";
+        StringBuilder password = new StringBuilder();
 
         // constructing password as a string
         for (char c : input) {
-            password += c;
+            password.append(c);
         }
-        System.out.println(password);
-        if (password.equals("test")) System.out.println("YES");
+
+        try {
+            // generate secure key for AES encryption
+            // source - https://stackoverflow.com/questions/51770704/java-aes-decryption-code-is-not-working-invalidexception-1234444
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            SecretKey secretKey = keyGenerator.generateKey();
+
+            byte[] iv = new byte[16]; // 16 bytes long
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv); // set up iv
+
+            // System.out.println(secretKey);
+            System.out.println("What you typed: " + password);
+            String encrypted = encryptPassword(password.toString(), secretKey, ivParameterSpec);
+            decryptPassword(encrypted, secretKey, ivParameterSpec);
+
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+
+
+        if (!checkPassword(password.toString())) {
+            // update text
+            result.setText("Wrong Password");
+        } else {
+            // give access and close window
+            result.setText("Correct Password");
+            this.success = true;
+            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); // close window after giving access
+        }
     }
 
-    public static void main(String[] args)   {
-        Authentication passwordEntry = new Authentication();
+    public boolean isSuccess() {
+        return this.success;
     }
-
 }

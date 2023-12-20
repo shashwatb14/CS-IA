beee6d9d48305941d79a4ca47436ca34550e00e1
Shashwat <shashwat.bhandari.14@gmail.com>
2023-12-20 17:31:19 +0800

    Complete select and refactor methods

--- DatabaseHandler.java
+++ DatabaseHandler.java
@@ -7,7 +7,9 @@
  */
 
 import java.sql.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DatabaseHandler {
 
@@ -22,6 +24,7 @@ public class DatabaseHandler {
         Connection connection;
 
         // try-catch instead of adding exceptions to method signature
+        // no need for try-catch in Main
         // ClassNotFoundException, SQLException
         try {
             Class.forName("org.sqlite.JDBC");
@@ -36,15 +39,15 @@ public class DatabaseHandler {
     // method for selection
     // selects all records from chosen table - only for viewing purposes
     public void selectAll(String tableName) {
+
+        String sql = "SELECT * FROM " + tableName;
+
         try {
-            String sql = "SELECT * FROM " + tableName;
-            Statement statement = this.connection.createStatement();
-            ResultSet results = statement.executeQuery(sql);
+            ResultSet results = this.connection.createStatement().executeQuery(sql);
 
             // getting column count
             // https://www.ibm.com/docs/en/db2-for-zos/11?topic=applications-learning-about-resultset-using-resultsetmetadata-methods
-            ResultSetMetaData resultsMetaData = results.getMetaData();
-            int columnCount = resultsMetaData.getColumnCount();
+            int columnCount = results.getMetaData().getColumnCount();
 
             StringBuilder result = new StringBuilder();
             for (int i = 1; i <= columnCount; i++) {
@@ -57,14 +60,43 @@ public class DatabaseHandler {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
     }
 
-    // selects specific columns based on argument
+    // selects specific columns based on argument and
+    public Map[] select(String tableName, String[] columns) {
 
+        // build column argument for sql statement
+        StringBuilder columnList = new StringBuilder();
+        columnList.append(columns[0]);
+        for (int i = 1, n = columns.length; i < n; i++) {
+            columnList.append(", ").append(columns[i]);
+        }
+
+        String sql = "SELECT " + columnList + " FROM " + tableName;
+
+        try {
+            ResultSet resultSet = this.connection.createStatement().executeQuery(sql);
+            Map[] records = new Map[resultSet.getMetaData().getColumnCount()];
+
+            for (int i = 0; resultSet.next(); i++) {
+                Map<String, String> results = new HashMap<>();
+
+                // enhanced for loop
+                for (String column : columns) {
+                    results.put(column, resultSet.getString(column));
+                }
+                records[i] = results;
+            }
+            return records;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     // method for insertion
     // multiple data types storage - https://stackoverflow.com/questions/26162183/java-multiple-type-data-structure
-    public void insert(String tableName, String columns, List<Object> values) throws SQLException {
+    public void insert(String tableName, String columns, List<Object> values) {
 
         // values list
         StringBuilder valueList = new StringBuilder();
@@ -76,7 +108,11 @@ public class DatabaseHandler {
         String sql = "INSERT INTO " + tableName + "(" + columns + ") VALUES(" + valueList + ")";
         System.out.println(sql);
 
-        PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // method for deletion

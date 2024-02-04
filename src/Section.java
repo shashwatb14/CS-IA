ff5c5fa6fda4a6c793c00347b661129dfa3cd3c4
Shashwat <shashwat.bhandari.14@gmail.com>
2024-02-04 16:01:42 +0800

    Add edit and archive methods

--- src/Section.java
+++ src/Section.java
@@ -9,14 +9,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Section implements ActionListener {
 
     private final boolean LOCKED;
 
-    private final JButton MAIN_BUTTON = new JButton(); // clickable panels
     private final JButton EDIT_BUTTON = new JButton("Edit");
     private final JButton ARCHIVE_BUTTON = new JButton("Archive");
     private final DatabaseHandler DATABASE;
@@ -27,6 +28,8 @@ public class Section implements ActionListener {
     // access to cards Panel
     private final JPanel CARDS;
 
+    private final int SECTION_ID;
+
 
     public Section(String title, boolean locked, JPanel panel, JPanel cards, DatabaseHandler database) {
 
@@ -35,7 +38,18 @@ public class Section implements ActionListener {
         this.CARDS = cards;
         this.DATABASE = database;
 
+        try {
+            this.SECTION_ID = DATABASE.customStatement(
+                    "sections",
+                    "SELECT id FROM sections WHERE sectionTitle = \"" + this.SECTION_NAME + "\""
+            ).getInt("id");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         // styling buttons: https://stackoverflow.com/questions/4898584/java-using-an-image-as-a-button
+        // clickable panels
+        JButton MAIN_BUTTON = new JButton();
         MAIN_BUTTON.setContentAreaFilled(false);
         MAIN_BUTTON.setBorderPainted(false);
 
@@ -63,32 +77,7 @@ public class Section implements ActionListener {
         MAIN_BUTTON.addActionListener(this);
 
         // update cursor to pointer
-        MAIN_BUTTON.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                MAIN_BUTTON.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                MAIN_BUTTON.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                MAIN_BUTTON.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                MAIN_BUTTON.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                MAIN_BUTTON.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-        });
+        Main.changeCursor(MAIN_BUTTON, new Cursor(Cursor.HAND_CURSOR));
 
         buttonsPanel.add(EDIT_BUTTON);
         buttonsPanel.add(ARCHIVE_BUTTON);
@@ -103,6 +92,83 @@ public class Section implements ActionListener {
         return LOCKED;
     }
 
+    private void archive() {
+        // GUI frame
+        JFrame archiveFrame = new JFrame("Archive");
+
+        // main panel
+        JPanel mainPanel = new JPanel();
+
+        // panel for warnings
+        JPanel warnings = new JPanel();
+        warnings.setLayout(new BoxLayout(warnings, BoxLayout.Y_AXIS));
+
+
+        JPanel warningTitle = new JPanel();
+        JPanel warningPanel1 = new JPanel();
+        JPanel warningPanel2 = new JPanel();
+        JPanel warningPanel3 = new JPanel();
+
+        JLabel warning = new JLabel("WARNING");
+        warning.setForeground(Color.RED);
+        warningTitle.add(warning);
+
+        warningPanel1.add(new JLabel("Are you sure you want to archive this section?"));
+        warningPanel2.add(new JLabel("Sections cannot be restored through the application."));
+        warningPanel3.add(new JLabel("All notes will become inaccessible."));
+
+        warnings.add(warningTitle);
+        warnings.add(warningPanel1);
+        warnings.add(warningPanel2);
+        warnings.add(warningPanel3);
+
+        // panel for choice
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BorderLayout());
+
+        JButton confirmArchive = new JButton("Archive");
+        JButton cancelArchive = new JButton("Cancel");
+
+        confirmArchive.addActionListener(e -> {
+            List<Object> values = new ArrayList<>();
+            values.add(this.SECTION_ID);
+            values.add(this.SECTION_NAME);
+            values.add((this.LOCKED) ? "TRUE" : "FALSE");
+            DATABASE.insert("archive", "id, sectionTitle, isLocked", values);
+            DATABASE.delete("sections", this.SECTION_NAME);
+            Main.buildApplication();
+            archiveFrame.dispatchEvent(new WindowEvent(archiveFrame, WindowEvent.WINDOW_CLOSING));
+        });
+        cancelArchive.addActionListener(e -> archiveFrame.dispatchEvent(new WindowEvent(archiveFrame, WindowEvent.WINDOW_CLOSING)));
+
+        Main.changeCursor(confirmArchive, new Cursor(Cursor.HAND_CURSOR));
+        Main.changeCursor(cancelArchive, new Cursor(Cursor.HAND_CURSOR));
+
+        buttonPanel.add(confirmArchive, BorderLayout.WEST);
+        buttonPanel.add(cancelArchive, BorderLayout.EAST);
+
+        mainPanel.add(warnings);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        mainPanel.add(buttonPanel);
+
+        Main.buildDialogBox(archiveFrame, mainPanel);
+    }
+
+    // edit title and locked status of section
+    private void edit() {
+        Main.createSection(
+                Main.newSection(
+                        "Edit section",
+                        "Save changes",
+                        true,
+                        this.SECTION_NAME,
+                        this.LOCKED
+                ),
+                false,
+                this.SECTION_ID
+        );
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
@@ -114,11 +180,13 @@ public class Section implements ActionListener {
 
                 if (e.getSource() == EDIT_BUTTON) {
                     System.out.println("Editing " + this.SECTION_NAME);
+                    edit();
                 }
 
-                // delete section
+                // archive section
                 else if (e.getSource() == ARCHIVE_BUTTON) {
-                    System.out.println("Deleting " + this.SECTION_NAME);
+                    System.out.println("Archiving " + this.SECTION_NAME);
+                    archive();
                 }
 
 
@@ -134,11 +202,13 @@ public class Section implements ActionListener {
             // edit section title
             if (e.getSource() == EDIT_BUTTON) {
                 System.out.println("Editing " + this.SECTION_NAME);
+                edit();
             }
 
-            // delete section
+            // archive section
             else if (e.getSource() == ARCHIVE_BUTTON) {
-                System.out.println("Deleting " + this.SECTION_NAME);
+                System.out.println("Archiving " + this.SECTION_NAME);
+                archive();
             }
 
             // change view using CardLayout
